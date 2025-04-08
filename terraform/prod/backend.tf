84d51cf9a2c9e91b9bb62831962dc1dbc9e783ce
KennyH <kennyherrington@gmail.com>
2025-04-08 00:13:10 -0700

    make backend region string not variable

--- terraform/prod/backend.tf
+++ terraform/prod/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "mine-bitcoin-online-terraform-state"
     key            = "prod/terraform.tfstate"
-    region         = var.aws_region
+    region         = "us-west-2"
     dynamodb_table = "mine-bitcoin-online-terraform-state-lock"
     encrypt        = true
   }
