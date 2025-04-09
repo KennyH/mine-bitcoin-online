a74a9ffb50029713993d368fe678dccc49ddaa7a
KennyH <kennyherrington@gmail.com>
2025-04-09 02:44:59 -0700

    try more hacks on cognito

--- terraform/modules/btcminer_backend/cognito-local.tf
+++ terraform/modules/btcminer_backend/cognito-local.tf
@@ -6,7 +6,6 @@ resource "aws_cognito_user_pool_client" "user_pool_client_local_dev" {
   allowed_oauth_flows_user_pool_client = true
 
   explicit_auth_flows = [
-    "ALLOW_USER_SRP_AUTH",
     "ALLOW_REFRESH_TOKEN_AUTH"
   ]
 
