d343422baa10bebbd8412fd8cea03d5579e44fc9
KennyH <kennyherrington@gmail.com>
2025-04-11 09:38:01 -0700

    fix output description

--- src/backend/terraform/modules/website/outputs.tf
+++ src/backend/terraform/modules/website/outputs.tf
@@ -1,4 +1,4 @@
 output "cloudfront_distribution_url" {
-  description = "CloudFront distribution domain name for ${var.project_name}"
+  description = "CloudFront distribution domain name for the website"
   value       = aws_cloudfront_distribution.frontend_distribution.domain_name
 }
