cb9cb714742404281e2712ced28486919f53a41e
KennyH <kennyherrington@gmail.com>
2025-04-27 22:47:56 -0700

    do it in parts; cloudfront first

--- src/backend/terraform/modules/website/s3.tf
+++ src/backend/terraform/modules/website/s3.tf
@@ -23,10 +23,10 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
     Statement = [{
       Effect = "Allow"
       ## Not using OAI
-      # Principal = {
-      #   AWS = aws_cloudfront_origin_access_identity.origin_identity.iam_arn
-      # }
-      Principal = "*"
+      Principal = {
+        AWS = aws_cloudfront_origin_access_identity.origin_identity.iam_arn
+      }
+      # Principal = "*"
       Action   = ["s3:GetObject"]
       Resource = ["${aws_s3_bucket.frontend_bucket.arn}/*"]
     }]
@@ -34,22 +34,22 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
 }
 
 ## Not using OAI, so the bucket needs to be public. 
-# resource "aws_s3_bucket_public_access_block" "public_block" {
-#   bucket                  = aws_s3_bucket.frontend_bucket.id
-#   block_public_acls       = true
-#   block_public_policy     = true
-#   ignore_public_acls      = true
-#   restrict_public_buckets = true
-# }
-
-resource "aws_s3_bucket_website_configuration" "frontend_bucket_website" {
-  bucket = aws_s3_bucket.frontend_bucket.id
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "404.html"
-  }
+resource "aws_s3_bucket_public_access_block" "public_block" {
+  bucket                  = aws_s3_bucket.frontend_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
+
+# resource "aws_s3_bucket_website_configuration" "frontend_bucket_website" {
+#   bucket = aws_s3_bucket.frontend_bucket.id
+
+#   index_document {
+#     suffix = "index.html"
+#   }
+
+#   error_document {
+#     key = "404.html"
+#   }
+# }
