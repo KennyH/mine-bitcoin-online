add47a910004ea992441ff45be31e2f5b70bc3f1
KennyH <kennyherrington@gmail.com>
2025-04-28 15:57:53 -0700

    change block public to false

--- src/backend/terraform/modules/website/s3.tf
+++ src/backend/terraform/modules/website/s3.tf
@@ -35,7 +35,7 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
 resource "aws_s3_bucket_public_access_block" "public_block" {
   bucket                  = aws_s3_bucket.frontend_bucket.id
   block_public_acls       = true
-  block_public_policy     = true
+  block_public_policy     = false ##
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
