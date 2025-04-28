c9c3524828a6ace4a48624a85eea0831fc81b2b0
KennyH <kennyherrington@gmail.com>
2025-04-28 04:15:08 -0700

    add new cloudfront function

--- src/backend/terraform/modules/website/cloudfront_function.tf
+++ src/backend/terraform/modules/website/cloudfront_function.tf
@@ -1,50 +1,30 @@
-# resource "aws_cloudfront_function" "redirect_and_add_index_html" {
-#   name    = "add-index-html-for-mine-bitcoin-online-frontend-${var.environment}"
-#   runtime = "cloudfront-js-1.0"
-#   publish = true
+resource "aws_cloudfront_function" "redirect_and_add_index_html" {
+  name    = "redirect-and-add-index-html-for-mine-bitcoin-online-frontend-${var.environment}"
+  runtime = "cloudfront-js-1.0"
+  publish = true
 
-#   code = <<EOT
-# function handler(event) {
-#     var request = event.request;
-#     var uri = request.uri;
+  code = <<EOT
+function handler(event) {
+    var request = event.request;
+    var uri = request.uri;
 
-#     // Normalize multiple slashes
-#     uri = uri.replace(/\/{2,}/g, '/');
+    // Normalize multiple slashes
+    uri = uri.replace(/\/{2,}/g, '/');
 
-#     // Remove all trailing slashes
-#     uri = uri.replace(/\/+$/, '');
+    // Remove all trailing slashes
+    uri = uri.replace(/\/+$/, '');
 
-#     // If URI is empty, set it to "/"
-#     if (uri === '') {
-#         uri = '/';
-#     }
+    // If URI is empty, set it to "/"
+    if (uri === '') {
+        uri = '/';
+    }
 
-#     if (!uri.endsWith('/')) {
-#         uri += '/';
-#     }
+    if (!uri.endsWith('/')) {
+        uri += '/';
+    }
 
-#     request.uri = uri + 'index.html';
-#     return request;
-# }
-# EOT
-# }
-
-
-resource "aws_cloudfront_function" "add_index_html" {
-   name    = "add-index-html-for-mine-bitcoin-online-frontend-${var.environment}"
-   runtime = "cloudfront-js-1.0"
-   publish = true
- 
-   code = <<EOT
- function handler(event) {
-     var request = event.request;
-     var uri = request.uri;
- 
-     if (uri.endsWith('/')) {
-         request.uri += 'index.html';
-     }
- 
-     return request;
- }
- EOT
- }
+    request.uri = uri + 'index.html';
+    return request;
+}
+EOT
+}
