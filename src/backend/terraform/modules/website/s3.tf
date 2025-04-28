8dcc64a2e35aa5903622c207e24a55f404f14e58
KennyH <kennyherrington@gmail.com>
2025-04-28 00:19:40 -0700

    turn off public policy flags

--- src/backend/terraform/modules/website/s3.tf
+++ src/backend/terraform/modules/website/s3.tf
@@ -36,10 +36,10 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
 ## Not using OAI, so the bucket needs to be public. 
 resource "aws_s3_bucket_public_access_block" "public_block" {
   bucket                  = aws_s3_bucket.frontend_bucket.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 # resource "aws_s3_bucket_website_configuration" "frontend_bucket_website" {
