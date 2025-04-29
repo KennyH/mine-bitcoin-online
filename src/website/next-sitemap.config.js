bc92b5a56fe1d2890268cde3778af9768dc9aef9
KennyH <kennyherrington@gmail.com>
2025-04-28 19:15:10 -0700

    sitemap and allow robots only on prod

--- src/website/next-sitemap.config.js
+++ src/website/next-sitemap.config.js
@@ -1,8 +1,24 @@
+const isProd = process.env.NEXT_PUBLIC_ENVIRONMENT === 'prod';
+
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
     siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://dev-env.bitcoinbrowserminer.com', // fallback
-    //generateRobotsTxt: true,
-    outDir: 'out',
-    //exclude: process.env.NEXT_PUBLIC_ENVIRONMENT === 'dev' ? ['/'] : [],
-  }
-  
+    generateRobotsTxt: true,
+    generateIndexSitemap: isProd,
+    exclude: isProd ? [] : ['/**'],
+    robotsTxtOptions: {
+        policies: isProd
+        ? [
+            { userAgent: '*', allow: '/' },
+          ]
+        : [
+            { userAgent: '*', disallow: '/' },
+            { userAgent: 'GPTBot', disallow: '/' },
+            { userAgent: 'ChatGPT-User', disallow: '/' },
+            { userAgent: 'Googlebot', disallow: '/' },
+            { userAgent: 'Bingbot', disallow: '/' },
+            { userAgent: 'anthropic-ai', disallow: '/' },
+            { userAgent: 'ClaudeBot', disallow: '/' },
+          ],
+    },
+};
