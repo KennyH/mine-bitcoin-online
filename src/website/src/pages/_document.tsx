3371ede5edece1bd3463f7e5c912635dc9e18204
Kenny <kennyherrington@gmail.com>
2025-04-29 13:44:00 -0700

    fix fonts on windows

--- src/website/src/pages/_document.tsx
+++ src/website/src/pages/_document.tsx
@@ -2,12 +2,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en" className="min-h-screen">
+    <Html lang="en" className={`min-h-screen ${""}`}>
       <Head>
         <meta name="description" content="Browser-based Bitcoin mining lottery." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <body className="min-h-screen flex flex-col bg-[#0a0c10] text-[#e0e6ed] font-sans">
+      <body className={"min-h-screen flex flex-col bg-[#0a0c10] text-[#e0e6ed]"}>
         <Main />
         <NextScript />
       </body>
