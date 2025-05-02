aad37f5702924cdae5d2ff9cd0951b0532b44f00
KennyH <kennyherrington@gmail.com>
2025-05-02 14:14:15 -0700

    fix output scoping in root paths

--- src/backend/terraform/modules/website/outputs.tf
+++ src/backend/terraform/modules/website/outputs.tf
@@ -1,9 +1,9 @@
 output "cloudfront_distribution_url" {
   description = "CloudFront distribution domain name for the website"
-  value       = aws_cloudfront_distribution.frontend_distribution.domain_name
+  value       = module.website.cloudfront_domain_name
 }
 
 output "cf_turnstile_api_endpoint" {
   description = "The invoke URL for the Cloudflare Turnstile verification API endpoint"
-  value       = "${aws_apigatewayv2_stage.cf_turnstile_default_stage.invoke_url}/verify"
+  value       = module.website.cf_turnstile_api_endpoint
 }
