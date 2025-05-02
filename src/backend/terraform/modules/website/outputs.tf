5c67fda9630573f62a4b238ab3196f2a7e9143a5
KennyH <kennyherrington@gmail.com>
2025-05-02 14:18:17 -0700

    fix the right outputs file

--- src/backend/terraform/modules/website/outputs.tf
+++ src/backend/terraform/modules/website/outputs.tf
@@ -1,9 +1,9 @@
 output "cloudfront_distribution_url" {
   description = "CloudFront distribution domain name for the website"
-  value       = module.website.cloudfront_domain_name
+  value       = aws_cloudfront_distribution.frontend_distribution.domain_name
 }
 
 output "cf_turnstile_api_endpoint" {
   description = "The invoke URL for the Cloudflare Turnstile verification API endpoint"
-  value       = module.website.cf_turnstile_api_endpoint
+  value       = "${aws_apigatewayv2_stage.cf_turnstile_default_stage.invoke_url}/verify"
 }
