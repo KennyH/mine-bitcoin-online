e294efeac50ad35861cc0efa3bb3e7e135a9809c
KennyH <kennyherrington@gmail.com>
2025-05-16 05:09:53 -0700

    demo

--- src/backend/terraform/dev/s3.tf
+++ src/backend/terraform/dev/s3.tf
@@ -5,6 +5,7 @@ resource "aws_s3_bucket" "demo_website_bucket" {
     Environment = "Demo"
     Project     = "VulnerabilityMVP"
   }
+  force_destroy = true
 }
 
 resource "aws_s3_bucket_website_configuration" "demo_website_config" {
